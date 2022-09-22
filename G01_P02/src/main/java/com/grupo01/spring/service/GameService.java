9711fb65137c03d3ef1bc91912264c27b20b372d
Javier Cuenca Gento <100384012@alumnos.uc3m.es>
2022-09-22 22:49:07 +0200

    ListaGames pepino

--- G01_P02/src/main/java/com/grupo01/spring/service/GameService.java
+++ G01_P02/src/main/java/com/grupo01/spring/service/GameService.java
@@ -1,5 +1,9 @@
 package com.grupo01.spring.service;
 
+import java.util.List;
+
+import com.grupo01.spring.model.Game;
+
 /**
  * 
  * @author Grupo 01
@@ -7,5 +11,5 @@ package com.grupo01.spring.service;
  */
 
 public interface GameService {
-
+	public List<Game> findAll();
 }
